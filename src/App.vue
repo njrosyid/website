e2ad967e3699ad5ae29478cbd708d5067cdd4246
Hendra Dwi Saputra <hamdakom@gmail.com>
2017-08-28 00:00:06 +0700

    latihan1
    halo dunia & input v-model

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="nama"> {{ nama }}
+    <pre>{{ $data | json}}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      nama: 'rosyid'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
